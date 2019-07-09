66191f88037905e7d0eac7892cb1c9b7f0a6235a
Kouhei Sano <sano@buddying.jp>
2019-07-09 12:49:39 +0900

    feat: line bot

---
+++ public/line/line.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::create(__DIR__ . '/../');
+$dotenv->load();
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('LINE_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('LINE_CHANNEL_SECRET')]);
+
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+foreach ($events as $event) {
+    $response = $bot->replyMessage(
+        $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('HELLO')
+    );
+}
